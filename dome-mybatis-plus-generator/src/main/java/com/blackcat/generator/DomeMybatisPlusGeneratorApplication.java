b9bd820e628feabf396507fc29e7250586a45cd2
kylin_lawliet <kylin_lawliet@163.com>
2020-01-07 12:59:13 +0800

    MyBatis-Plus 代码生成器 示例

---
+++ dome-mybatis-plus-generator/src/main/java/com/blackcat/generator/DomeMybatisPlusGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.blackcat.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomeMybatisPlusGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomeMybatisPlusGeneratorApplication.class, args);
+    }
+
+}
