a1edbd7244b10cc29c680c7a47a5fa351f8a0d6c
kylin_lawliet <kylin_lawliet@163.com>
2020-01-07 12:49:47 +0800

    springboot项目搭建

---
+++ blog-base/src/main/java/com/blackcat/base/BlogBaseApplication.java
@@ -0,0 +1,13 @@
+package com.blackcat.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBaseApplication.class, args);
+    }
+
+}
