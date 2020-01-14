797a0ca9b1dc7eab0a08492c4fb4290ed7a2f92d
kylin_lawliet <kylin_lawliet@163.com>
2020-01-14 19:56:02 +0800

    添加扫描

--- blog-base/src/main/java/com/blackcat/blog/BlogBaseApplication.java
+++ blog-base/src/main/java/com/blackcat/blog/BlogBaseApplication.java
@@ -1,9 +1,11 @@
-package com.blackcat.base;
+package com.blackcat.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.blackcat.blog.core.dao"}) //扫描DAO
 public class BlogBaseApplication {
 
     public static void main(String[] args) {
